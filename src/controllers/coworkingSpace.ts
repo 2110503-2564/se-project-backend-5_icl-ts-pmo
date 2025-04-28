08733d81c487adf5752fcf19bf730b2ee78b7bc7
Cookie4686 <cookieandcake1105@gmail.com>
2025-04-28 21:27:49 +0700

    add dashboard route,controller

--- src/controllers/coworkingSpace.ts
+++ src/controllers/coworkingSpace.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from "express";
 import dbConnect from "../dbConnect.js";
 import CoworkingSpace from "../models/CoworkingSpace.js";
-import Reservation from "../models/Reservation.js";
+import Reservation, { ReservationType } from "../models/Reservation.js";
+import mongoose, { Types } from "mongoose";
 
 export const getCoWorkingSpaces: RequestHandler = async (req, res) => {
   await dbConnect();
@@ -81,6 +82,183 @@ export const deleteCoWorkingSpace: RequestHandler = async (req, res) => {
       res.status(200).json({ success: true });
     } else {
       res.status(404).json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false });
+  }
+};
+
+export const getCoworkingSpaceTotalReservation: RequestHandler = async (req, res) => {
+  await dbConnect();
+  try {
+    const result = (
+      await Reservation.aggregate<
+        ({ [P in ReservationType["approvalStatus"]]: number } & { total: number }) | undefined
+      >([
+        {
+          $facet: {
+            data: [
+              { $match: { coworkingSpace: mongoose.Types.ObjectId.createFromHexString(req.params.id) } },
+              { $group: { _id: "$approvalStatus", count: { $count: {} } } },
+              {
+                $group: {
+                  _id: null,
+                  items: { $push: { k: "$_id", v: "$count" } },
+                  total: { $sum: "$count" },
+                },
+              },
+              {
+                $replaceRoot: {
+                  newRoot: { $mergeObjects: [{ $arrayToObject: "$items" }, { total: "$total" }] },
+                },
+              },
+            ],
+          },
+        },
+        { $replaceRoot: { newRoot: { $ifNull: [{ $arrayElemAt: ["$data", 0] }, { total: 0 }] } } },
+        {
+          $set: {
+            approved: { $ifNull: ["$approved", 0] },
+            pending: { $ifNull: ["$pending", 0] },
+            canceled: { $ifNull: ["$canceled", 0] },
+            rejected: { $ifNull: ["$rejected", 0] },
+          },
+        },
+      ])
+    )[0];
+    if (result) {
+      res.status(200).json({ success: true, data: result });
+    } else {
+      res.status(500).json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false });
+  }
+};
+
+export const getCoworkingSpaceFrequency: RequestHandler = async (req, res) => {
+  await dbConnect();
+  try {
+    const result = (
+      await Reservation.aggregate<{ data: { label: string; data: number[] }[]; label: string[] } | undefined>(
+        [
+          { $match: { coworkingSpace: mongoose.Types.ObjectId.createFromHexString(req.params.id) } },
+          {
+            $set: {
+              startDate: {
+                $dateFromParts: {
+                  year: { $year: "$startDate" },
+                  month: { $month: "$startDate" },
+                  day: { $dayOfMonth: "$startDate" },
+                  hour: { $hour: "$startDate" },
+                  minute: {
+                    $subtract: [{ $minute: "$startDate" }, { $mod: [{ $minute: "$startDate" }, 30] }],
+                  },
+                },
+              },
+              endDate: {
+                $dateFromParts: {
+                  year: { $year: "$endDate" },
+                  month: { $month: "$endDate" },
+                  day: { $dayOfMonth: "$endDate" },
+                  hour: { $hour: "$endDate" },
+                  minute: { $subtract: [{ $minute: "$endDate" }, { $mod: [{ $minute: "$endDate" }, 30] }] },
+                },
+              },
+            },
+          },
+          {
+            $replaceRoot: {
+              newRoot: {
+                data: {
+                  $cond: [
+                    { $expr: { $eq: ["$startDate", "$endDate"] } },
+                    [{ _id: "$_id", time: "$startDate" }],
+                    [
+                      { _id: "$_id", time: "$startDate" },
+                      { _id: "$_id", time: "$endDate" },
+                    ],
+                  ],
+                },
+              },
+            },
+          },
+          { $unwind: { path: "$data" } },
+          {
+            $densify: {
+              field: "data.time",
+              partitionByFields: ["data._id"],
+              range: { step: 30, unit: "minute", bounds: "partition" },
+            },
+          },
+          {
+            $lookup: { from: "reservations", localField: "data._id", foreignField: "_id", as: "reservation" },
+          },
+          { $unwind: { path: "$reservation" } },
+          {
+            $group: {
+              _id: {
+                $dateFromParts: { year: 1, hour: { $hour: "$data.time" }, minute: { $minute: "$data.time" } },
+              },
+              approved: { $sum: { $cond: [{ $eq: ["$reservation.approvalStatus", "approved"] }, 1, 0] } },
+              rejected: { $sum: { $cond: [{ $eq: ["$reservation.approvalStatus", "rejected"] }, 1, 0] } },
+              pending: { $sum: { $cond: [{ $eq: ["$reservation.approvalStatus", "pending"] }, 1, 0] } },
+              canceled: { $sum: { $cond: [{ $eq: ["$reservation.approvalStatus", "canceled"] }, 1, 0] } },
+            },
+          },
+          {
+            $densify: {
+              field: "_id",
+              range: {
+                step: 30,
+                unit: "minute",
+                bounds: [new Date("0001-01-01T00:00:00.000Z"), new Date("0001-01-02T00:00:00.000Z")],
+              },
+            },
+          },
+          {
+            $fill: {
+              sortBy: { _id: 1 },
+              output: {
+                approved: { value: 0 },
+                rejected: { value: 0 },
+                canceled: { value: 0 },
+                pending: { value: 0 },
+              },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              label: { $push: { $dateToString: { date: "$_id", format: "%H:%M" } } },
+              approved: { $push: "$approved" },
+              pending: { $push: "$pending" },
+              canceled: { $push: "$canceled" },
+              rejected: { $push: "$rejected" },
+            },
+          },
+          {
+            $replaceRoot: {
+              newRoot: {
+                data: [
+                  { label: "Approved", data: "$approved" },
+                  { label: "Rejected", data: "$rejected" },
+                  { label: "Pending", data: "$pending" },
+                  { label: "Canceled", data: "$canceled" },
+                ],
+                label: "$label",
+              },
+            },
+          },
+        ]
+      )
+    )[0];
+    if (result) {
+      res.status(200).json({ success: true, data: result });
+    } else {
+      res.status(500).json({ success: false });
     }
   } catch (error) {
     console.error(error);
--- src/routes/coworkingSpaces.ts
+++ src/routes/coworkingSpaces.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { protect, authorize, readToken } from "../middleware.js";
+import { protect, readToken } from "../middleware.js";
 import {
   createCoWorkingSpace,
   deleteCoWorkingSpace,
   getCoWorkingSpace,
+  getCoworkingSpaceFrequency,
   getCoWorkingSpaces,
+  getCoworkingSpaceTotalReservation,
   updateCoWorkingSpace,
 } from "../controllers/coworkingSpace.js";
 
@@ -16,6 +18,8 @@ router
   .get(readToken, getCoWorkingSpace)
   .put(readToken, protect, updateCoWorkingSpace)
   .delete(readToken, protect, deleteCoWorkingSpace);
+router.route("/:id/frequency").get(readToken, protect, getCoworkingSpaceFrequency);
+router.route("/:id/totalReservation").get(readToken, protect, getCoworkingSpaceTotalReservation);
 
 export default router;
 
